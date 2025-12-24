80e46d589fcac2f9a6b0c9964f6f3d3c560c64ca
JayaramJayakrishnan <jayaramjayakrishnan@gmail.com>
2025-12-25 00:35:10 +0530

    init: starter template & static UI

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="main">
+        <div className="bg-box-secondary">
+          <div className="bg-box-secondary-inner-tl"></div>
+          <div className="bg-box-secondary-inner-tr"></div>
+          <div className="bg-box-secondary-inner-bl"></div>
+          <div className="bg-box-secondary-inner-br"></div>
+        </div>
+        <div className="bg-box-primary"></div>
+        <h1 className="title">ELEVATE</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
